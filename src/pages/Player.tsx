5a760796ebc53a0fcb35214c9af8115218b42a23
Dhyon Keyllo <dhyon.kpm@hotmail.com>
2025-01-22 21:53:41 -0300

    feat: add tailwind config and player component

---
+++ src/pages/Player.tsx
@@ -0,0 +1,48 @@
+import { Header } from "./Header";
+import { Video } from "./Video";
+import { Module } from "./Module";
+import { MessageCircle } from "lucide-react";
+
+export function Player() {
+  return (
+    <div className="h-screen bg-zinc-950 text-zinc-50 flex justify-center items-center">
+      <div className="flex w-[1100px] flex-col gap-6">
+        <div className="flex items-center justify-between">
+          {/* * HEADER */}
+          <Header />
+
+          <button className="flex transition-colors items-center gap-2 rounded bg-violet-500 px-3 py-2 text-sm font-medium text-white hover:bg-violet-600">
+            <MessageCircle className="w-4 h-4" />
+            Deixar feedback
+          </button>
+        </div>
+
+        <main className="relative flex overflow-hidden rounded-lg border border-zinc-800 bg-zinc-900 shadow pr-80">
+          <div className="flex-1">
+            <Video />
+          </div>
+
+          <aside className="divide-y-2 divide-zinc-900 w-80 border-l border-zinc-800 bg-zinc-900 absolute top-0 bottom-0 right-0 overflow-y-scroll scrollbar-thin scrollbar-track-zinc-950 scrollbar-thumb-zinc-800">
+            <Module
+              moduleIndex={0}
+              title="Desvendando o Redux"
+              amountOfLessons={3}
+            />
+
+            <Module
+              moduleIndex={1}
+              title="Desvendando o Redux"
+              amountOfLessons={3}
+            />
+
+            <Module
+              moduleIndex={2}
+              title="Desvendando o Redux"
+              amountOfLessons={3}
+            />
+          </aside>
+        </main>
+      </div>
+    </div>
+  );
+}
