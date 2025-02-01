50ba52abf0a2ea3f75c6be0f4fc69bce805ddb21
Dhyon Keyllo <dhyon.kpm@hotmail.com>
2025-02-01 16:16:27 -0300

    feat: add Zustand for managing player state
    Add Zustand as a dependency to manage the player state in the video lesson navigation feature. Zustand provides a simple and efficient way to manage state in React applications. This commit also includes the addition of the `zustand` package to the `package.json` and `package-lock.json` files.

---
+++ src/zustand-store/index.ts
@@ -0,0 +1,74 @@
+import { create } from "zustand";
+
+interface Course {
+  id: number;
+  modules: Array<{
+    id: number;
+    title: string;
+    lessons: Array<{
+      id: string;
+      title: string;
+      duration: string;
+    }>;
+  }>;
+}
+
+export interface PlayerState {
+  course: Course | null;
+  currentModuleIndex: number;
+  currentLessonIndex: number;
+  isLoading: boolean;
+
+  play: (moduleAndLessonIndex: [number, number]) => void;
+  next: () => void;
+  load: () => Promise<void>;
+}
+
+export const useStore = create<PlayerState>((set, get) => {
+  return {
+    course: null,
+    currentModuleIndex: 0,
+    currentLessonIndex: 0,
+    isLoading: true,
+
+    load: async () => {
+      set({ isLoading: true });
+
+      const response = await fetch("/api/courses/1");
+      const course = await response.json();
+
+      set({ course, isLoading: false });
+    },
+
+    play: (moduleAndLessonIndex: [number, number]) => {
+      const [currentModuleIndex, currentLessonIndex] = moduleAndLessonIndex;
+
+      set({
+        currentModuleIndex,
+        currentLessonIndex,
+      });
+    },
+
+    next: () => {
+      const { currentModuleIndex, currentLessonIndex, course } = get();
+
+      const nextLessonIndex = currentLessonIndex + 1;
+      const nextLesson =
+        course?.modules[currentModuleIndex].lessons[nextLessonIndex];
+
+      if (nextLesson) {
+        set({ currentLessonIndex: nextLessonIndex });
+      } else {
+        const nextModuleIndex = currentModuleIndex + 1;
+        const nextModule = course?.modules[nextModuleIndex];
+
+        if (nextModule) {
+          set({
+            currentModuleIndex: nextModuleIndex,
+            currentLessonIndex: 0,
+          });
+        }
+      }
+    },
+  };
+});
